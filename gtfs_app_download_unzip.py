cc0fb0dd9f70f85bc1c93959b406954937abaf0f
Armaghan Kaviani <armaghankaviani@yahoo.com>
2021-08-08 20:52:09 +0200

    Analyzer: Argparse is used for CLI.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- gtfs_app_download_unzip.py
+++ gtfs_app_download_unzip.py
@@ -9,26 +9,42 @@ import sys
 from time import time
 from gtfs_book_modules import gtfs_book_feed_download
 from gtfs_book_modules import gtfs_book_feed_unzip
+import argparse
 
 
 def main():
-    #### Default Values
-    feed_url = "https://download.gtfs.de/germany/fv_free/latest.zip"
-    unzip_path = "gtfs_feed_01"
-    tmp_zip_file_name = "tmp_latest_gtfs.zip"
+    UrlFeed = "https://download.gtfs.de/germany/fv_free/latest.zip"
+    FilePath = "GTFSFeeds"
 
-    #### Action Start
-    arguments_number = len(sys.argv) - 1
-    if arguments_number < 2:
-        print("Usage: python %s feed_url unzip_path" % sys.argv[0])
-        print("WARNING: 2 arguments expected, found only %s." % arguments_number)
-        print("         Using standard feed_url and unzip_path.")
-    else:
-        feed_url = sys.argv[1]
-        unzip_path = sys.argv[2]
+    TempZipFile = "TempZipFile.zip"
 
-    print("Download from: %s." % feed_url)
-    print("Unzip into: %s." % unzip_path)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u',
+                        action='store',
+                        dest='UrlFeed',
+                        default="",
+                        required=False,
+                        help='Set url_feed Manager')
+
+    parser.add_argument('-p',
+                        action='store',
+                        dest='FilePath',
+                        default="",
+                        required=False,
+                        help='Set PathFile Manager')
+
+    parser.add_argument('--version', action='version', version='1.0.0')
+    results = parser.parse_args()
+
+    if results.UrlFeed != "":
+        UrlFeed = results.UrlFeed
+
+    if results.FilePath != "":
+        FilePath = results.FilePath
+
+    print("Download from: %s." % UrlFeed)
+    print("Unzip into: %s." % FilePath)
 
     print("Start action.")
     start_time = time()
